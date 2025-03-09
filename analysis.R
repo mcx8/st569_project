6df8f2a21210c059d5ad3d73e46445e825bb1451
Maxine Cruz <terese.cruz@gmail.com>
2025-03-09 14:27:05 -0700

    Make mouse map

--- analysis.R
+++ analysis.R
@@ -3,19 +3,70 @@
 
 # Date created: 13 February 2025
 
-# Last modified: 13 February 2025
+# Last modified: 4 March 2025
 
 
 
-# --- ABOUT ---
+
+# ----- ABOUT -----
 
 # Contains script for analyzing final project data
 
 
 
-# --- 
+
+# ----- SET WORKING DIRECTORY -----
+
+# Set the working directory to start at st569_project
+
+
+
+
+# ----- LOAD LIBRARIES -----
+
+# For plotting
+library(ggplot2)
+library(leaflet)
+
+# For saving maps
+library(mapview)
+
+
+
+
+# ----- LOAD DATA -----
+
+# Read data set
+data <- read.csv("data/gbif/cleaned_species.csv")
+
+
+
+
+# ----- CREATE SPECIES OCCURRENCE MAP -----
 
 # Create map of data
+spp_plot <- leaflet(options = leafletOptions(zoomControl = FALSE,
+                                             attributionControl = FALSE)) %>%
+  addCircleMarkers(data = data,
+                   color = "#CD1076",
+                   radius = 5,
+                   fillOpacity = 0.8,
+                   stroke = FALSE) %>%
+  addProviderTiles("OpenStreetMap.Mapnik")
 
-# Semivariogram etc.
+
+# Check
+spp_plot
+
+# Save map
+mapshot(spp_plot,
+        file = "output/mouse_map.png")
+
+
+
+
+# ----- COMPLETE SPATIAL RANDOMNESS -----
+
+
+
 
